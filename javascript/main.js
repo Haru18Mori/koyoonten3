cde49a04e96d11671a5e0196ee648bf286029c93
Haru18Mori <119049595+Haru18Mori@users.noreply.github.com>
2023-09-05 23:25:39 +0900

    デザイン改良

--- javascript/main.js
+++ javascript/main.js
@@ -11,22 +11,3 @@ mask.addEventListener("click",function(){
     menu.classList.remove("active");
 })
 
-const swiper = new Swiper(".swiper", {
-    slidesPerView: 1,
-    breakpoints: {
-        // 768px以上の場合
-        768: {
-        slidesPerView: 3
-        }
-    },
-    // ページネーションが必要なら追加
-    pagination: {
-        el: ".swiper-pagination"
-    },
-    // ナビボタンが必要なら追加
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
-    
-});
